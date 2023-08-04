2fc8fb3ffbbde1abb47b4dc17577a26c40f60fb2
EricRosenhein <128406706+EricRosenhein@users.noreply.github.com>
2023-08-04 15:13:23 -0400

    FINAL

---
+++ BrandConsoleApp/Controller/AddBrandController.cs
@@ -0,0 +1,76 @@
+﻿using BrandConsoleApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrandConsoleApp.Controller
+{
+    public class AddBrandController
+    {
+
+        protected ICallback Observer;
+
+        public AddBrandController(ICallback obs) { this.Observer = obs; }
+
+        public void Execute(Dictionary<string, Object> incomingStateInfo, string name, string notes)
+        {
+            Dictionary<string, Object> stateInfo = new Dictionary<string, Object>();
+            string loginName = null;
+
+            try
+            {
+                loginName = incomingStateInfo["LoginName"].ToString();
+
+                if ( loginName != null)
+                {
+                    string username = incomingStateInfo["LoginName"].ToString();
+                    if ((username != null) && (username.Length > 0))
+                    {
+                        Brand someBrand = new Brand(name, notes);
+
+                        someBrand.Save();
+
+                        stateInfo["Message"] = " Brand added successfully!";
+                        stateInfo["LoginName"] = loginName;
+                        stateInfo["Brand"] = someBrand;
+                        Observer.OnCallback("AddBrandSuccess", stateInfo);
+
+
+                    }
+                    else
+                    {
+                        stateInfo["Message"] = " No login credentials found!";
+                        stateInfo["Brand"] = null;
+                        stateInfo["LoginName"] = loginName;
+                        Observer.OnCallback("AddBrandFailed", stateInfo);
+                    }
+                }
+                else
+                {
+                    stateInfo["Message"] = " No login credentials found!";
+                    stateInfo["Brand"] = null;
+                    stateInfo["LoginName"] = loginName;
+                    Observer.OnCallback("AddBrandFailed", stateInfo);
+                }
+            }
+            catch (KeyNotFoundException Excep)
+            {
+                stateInfo["Message"] = " No login credentials found!";
+                stateInfo["Brand"] = null;
+                stateInfo["LoginName"] = loginName;
+                Observer.OnCallback("AddBrandFailed", stateInfo);
+            }
+            catch (Exception Ex)
+            {
+
+                stateInfo["Message"] = " No login credentials found!";
+                stateInfo["Brand"] = null;
+                stateInfo["LoginName"] = loginName;
+                Observer.OnCallback("AddBrandFailed", stateInfo);
+            }
+               
+        }
+    }
+}
